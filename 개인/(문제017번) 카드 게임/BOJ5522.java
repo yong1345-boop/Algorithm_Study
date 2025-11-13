2e6fcca2635add6eecce6ac5d239ceaa26f838f9
kairos <yong1345@gmail.com>
2025-11-13 12:16:57 +0900

    Update BOJ5522.java

--- 개인/(문제017번) 카드 게임/BOJ5522.java
+++ 개인/(문제017번) 카드 게임/BOJ5522.java
@@ -4,15 +4,15 @@ public class CardGame {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    int sum = 0; // 총점 저장 변수
+    int sum = 0; // JOI군의 총점을 저장할 변수
 
-    // 5번 반복하며 점수 입력받아 합산
-    for (int i = 0; i < 5; i++) {
-      int score = sc.nextInt(); // i번째 점수
-      sum += score;             // 합계에 더함
+    // 문제에서 5번의 점수가 주어진다고 명시함
+    for (int i = 1; i <= 5; i++) {
+      int score = sc.nextInt(); // i번째 게임의 점수 (0 ≤ Ai ≤ 100)
+      sum += score;             // 점수 누적
     }
 
-    // 총점 출력
+    // JOI군의 총점 출력
     System.out.println(sum);
 
     sc.close();
