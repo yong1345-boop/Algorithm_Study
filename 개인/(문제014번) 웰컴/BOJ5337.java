67510ad97791f9625ea1ab320dbfb65f1956aa0d
kairos <yong1345@gmail.com>
2025-11-13 11:47:41 +0900

    Create BOJ5337.java

---
+++ 개인/(문제014번) 웰컴/BOJ5337.java
@@ -0,0 +1,9 @@
+public class Welcome {
+  public static void main(String[] args) {
+
+    System.out.println(".  .   .");
+    System.out.println("|  | _ | _. _ ._ _  _");
+    System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+
+  }
+}
