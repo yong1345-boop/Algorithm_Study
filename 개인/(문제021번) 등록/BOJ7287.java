a48cb2f7a9a23e5d67202cf6453e9c84a422a646
kairos <yong1345@gmail.com>
2025-11-14 15:57:38 +0900

    Create BOJ7287.java

---
+++ 개인/(문제021번) 등록/BOJ7287.java
@@ -0,0 +1,9 @@
+public class Register {
+  public static void main(String[] args) {
+    // 첫 줄: BOJ에서 네가 맞은 문제 수
+    System.out.println(123); // ← 여기를 네가 맞은 문제 수로 바꿔라
+
+    // 둘째 줄: 네 BOJ 아이디
+    System.out.println("your_boj_id"); // ← 여기를 네 아이디로 바꿔라
+  }
+}
