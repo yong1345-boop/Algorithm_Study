ed7aa534cf878bdd90cafc00a2a321fee4b54940
kairos <yong1345@gmail.com>
2025-12-07 21:30:08 +0900

    Add cat ASCII art program for BOJ 10171

---
+++ 개인/(문제031번) 고양이/BOJ10171.java
@@ -0,0 +1,12 @@
+/**
+ * 고양이 출력 (BOJ 10171)
+ */
+public class b031Cat {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
