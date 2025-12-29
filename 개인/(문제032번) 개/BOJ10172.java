2f805f032bc29d3a4643013a9d1fb3ab8fbe00e5
kairos <yong1345@gmail.com>
2025-12-29 22:05:34 +0900

    Refactor Main class to b032Dog with main method

--- 개인/(문제032번) 개/BOJ10172.java
+++ 개인/(문제032번) 개/BOJ10172.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void b032Dog(String[] args) {
+public class b032Dog {
+    public static void main(String[] args) {
         System.out.println("|\\_/|");
         System.out.println("|q p|   /}");
         System.out.println("( 0 )\"\"\"\\");
